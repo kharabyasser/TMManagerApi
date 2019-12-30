85ffe194d213e9830da4d28ed38881ab67f695c6
Yasser Kharab <42950969+kharabyasser@users.noreply.github.com>
2019-12-30 10:12:09 -0500

    Adding Azure logger

--- TMManagerApi/Program.cs
+++ TMManagerApi/Program.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace TMManagerApi
@@ -14,7 +8,13 @@ namespace TMManagerApi
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)
+                .ConfigureLogging((logging) =>
+                {
+                    logging.AddEventSourceLogger();
+                })
+                .Build()
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
